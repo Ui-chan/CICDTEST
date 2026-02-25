f2b1236acb111a6014bfe4fa8ed96f8e7b715b09
uc <an@ucui-MacBookPro.local>
2026-02-25 20:49:58 +0900

    feat: secret name

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+# 환경 변수 'APP_MESSAGE'를 읽어오고, 없으면 기본값 출력
+WELCOME_MSG = os.getenv("APP_MESSAGE", "Default Welcome Message")
+
+@get("/")
 def read_root():
-    return {"message": "CI/CD docker Pipeline Active!!21"}    
+    return {"message": WELCOME_MSG}
