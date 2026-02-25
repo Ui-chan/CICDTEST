87afdc48a4baa51e5c23923b281a2b265be929ca
uc <an@ucui-MacBookPro.local>
2026-02-25 20:51:37 +0900

    fix: app.get

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 # 환경 변수 'APP_MESSAGE'를 읽어오고, 없으면 기본값 출력
 WELCOME_MSG = os.getenv("APP_MESSAGE", "Default Welcome Message")
 
-@get("/")
+@app.get("/")  # 이 부분에 'app.'이 꼭 들어가야 합니다!
 def read_root():
     return {"message": WELCOME_MSG}
